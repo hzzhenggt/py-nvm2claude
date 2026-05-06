93ed75a6b8a978c9ee42d17d53d809f851c2843a
跳跳跳 <hzzgt>
2026-05-06 16:15:26 +0800

    添加 启动测试和AI 操作手册

--- pynvmproxy.py
+++ pynvmproxy.py
@@ -871,3 +871,4 @@ if __name__ == "__main__":
 """)
 
     app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
+
---
+++ start.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+chcp 65001
+setlocal enabledelayedexpansion
+set SCRIPT_DIR=%~dp0
+echo %SCRIPT_DIR%
+
+set TAB1=cmd /k "cd /d %SCRIPT_DIR% && echo %cd% && python pynvmproxy.py"
+
+set TAB2=cmd /k "cd /d %SCRIPT_DIR% && type proxy.log "
+
+wt.exe new-tab -p "Command Prompt" %TAB1%; split-pane -p "Command Prompt" %TAB2%
+
+endlocal
